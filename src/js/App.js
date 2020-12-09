187d5df00c868de20eb37bbbd6a17a9674b3c5a6
TytoCorvus <brycecollins0@gmail.com>
2020-12-08 19:33:31 -0700

    Hid API key from git

---
+++ src/js/App.js
@@ -0,0 +1,16 @@
+import {Fragment} from "react"
+import NavBar from "./NavBar.js"
+
+
+function App() {
+
+  //This should return two major sections - the navbar with the various navigation buttons along the top, and the space for the map down below.
+
+  return (
+    <Fragment>
+      <NavBar></NavBar>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/js/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class NavBar extends Component{
+
+    render(){
+        
+
+        return(
+            <div className="center-align orange" style={{width: '100%'}}>
+                NavBar
+            </div>
+        );
+    }
+}
+
+export default NavBar;
