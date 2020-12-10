5391d2e6dc675b63df3b2eeb37f255d7b4c9356d
TytoCorvus <brycecollins0@gmail.com>
2020-12-09 20:03:51 -0700

    Another intermediary commit

--- src/js/MapManager.js
+++ src/js/MapManager.js
@@ -1,36 +1,48 @@
 import React, { Component } from "react";
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-function MapManager() {
-    const [map, setMap] = React.useState(null)
-   
-    const onLoad = React.useCallback(function callback(map) {
+class MapManager extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            map: null
+        }
+    }
+
+    onLoad = () => (map) => {
       const bounds = new window.google.maps.LatLngBounds();
-      map.fitBounds(bounds);
-      setMap(map)
-    }, [])
+      this.state.map.fitBounds(bounds);
+      this.setMap(map)
+    }
    
-    const onUnmount = React.useCallback(function callback(map) {
-      setMap(null)
-    }, [])
-   
-    return (
-      <LoadScript
-        googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-      >
-        <GoogleMap
-          mapContainerStyle={{width: '800px', height: '800px'}}
-          center={{lat: 10.745,
-                   lng: 10.523}}
-          zoom={10}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
-        </GoogleMap>
-      </LoadScript>
-    )
-  }
+    onUnmount = () => (map) => {
+      this.setMap(null)
+    }
+
+    setMap = (map) => {
+        this.setState({map: map})
+    }
+
+    render(){
+        return (
+            <LoadScript
+              googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+            >
+              <GoogleMap
+                mapContainerStyle={{width: '100%', height: '100%'}}
+                center={{lat: 10.745,
+                         lng: 10.523}}
+                zoom={10}
+                onLoad={this.onLoad}
+                onUnmount={this.onUnmount}
+              >
+                { /* Child components, such as markers, info windows, etc. */ }
+                <></>
+              </GoogleMap>
+            </LoadScript>
+          )
+    }
+}
 
 export default MapManager;
